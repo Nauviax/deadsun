9bd6a2a67d18478c3d6518939729a00ab65db3bf
Nauviax <nauviax@icloud.com>
2026-06-07 17:47:00 +1200

    Reverted lightning damage increase

--- data_stuff/fulgora_changes.lua
+++ data_stuff/fulgora_changes.lua
@@ -3,8 +3,4 @@
 -- This change is so accumulators are still needed to power the base between infrequent lightning strikes, and so steam is a likely requirement.
 data.raw["planet"]["fulgora"].lightning_properties.lightnings_per_chunk_per_tick = 0.0001 -- Vanilla is 0.00166...
 
--- Lightning damage buff (Funny)
-data.raw["lightning"]["lightning"].damage = 290 -- Player hp is 250, shields are 50.
-
 -- Changes to lightning power density text on space map in locale files.
-
